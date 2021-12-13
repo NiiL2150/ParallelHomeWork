779a465748918561b724904ebfebcef1fe6103a8
Nail Shamsudinov <nail.shamsudinov@gmail.com>
2021-12-13 22:29:24 +0400

    Add project files.

---
+++ ParallelHomeWork/Program.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ParallelHomeWork
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            {
+                Task<long> task = Task.Run(() =>
+                {
+                    int factNum = Int32.Parse(Console.ReadLine());
+                    if (factNum > 20) { factNum = 20; }
+                    long factRes = 1;
+                    Parallel.For(2, factNum + 1, (item) =>
+                    {
+                        factRes *= item;
+                    });
+                    return factRes;
+                });
+                task.Wait();
+                Console.WriteLine($"Factorial: {task.Result}");
+
+                Task<int> task1 = Task.Run(() =>
+                {
+                    string tmp = task.Result.ToString();
+                    return tmp.Length;
+                });
+                Task<int> task2 = Task.Run(() =>
+                {
+                    int tmp = 0;
+                    long taskResCopy = task.Result;
+                    Parallel.For(0, task1.Result, (item) =>
+                    {
+                        tmp += (int)(taskResCopy % 10);
+                        taskResCopy /= 10;
+                    });
+                    return tmp;
+                });
+                Task.WaitAll(task1, task2);
+                Console.WriteLine($"Count: {task1.Result}");
+                Console.WriteLine($"Sum: {task2.Result}");
+            }
+
+            Console.ReadKey();
+            Console.Clear();
+
+            {
+                object tmp = new object();
+                string strTmp = tmp as string;
+                int a = Int32.Parse(Console.ReadLine());
+                int b = Int32.Parse(Console.ReadLine());
+                Task task = Task.Run(() =>
+                {
+                    Parallel.For(a, b + 1, (item) =>
+                    {
+                        Parallel.For(1, 11, (item2) =>
+                        {
+                            lock (tmp)
+                            {
+                                strTmp += $"{item} * {item2} = {item * item2}\n";
+                            }
+                        });
+                    });
+                });
+                task.Wait();
+                strTmp = strTmp.SortStringLines();
+                Console.WriteLine(strTmp);
+                Parallel.Invoke(async () =>
+                {
+                    await File.WriteAllTextAsync("log.txt", strTmp);
+                });
+                task.Wait();
+            }
+
+            Console.ReadKey();
+            Console.Clear();
+            */
+            {
+                string file = "log.txt";
+                string str = File.ReadAllText(file);
+                List<int> list = str.ExtractNumbers();
+                List<long> longList = new List<long>();
+
+                Task task = Task.Run(() =>
+                {
+                    Parallel.ForEach(list, (item) =>
+                    {
+                        int factNum = item;
+                        long factRes = 1;
+                        Parallel.For(2, factNum + 1, (item2) =>
+                        {
+                            factRes *= item2;
+                        });
+                        longList.Add(factRes);
+                    });
+                });
+                task.Wait();
+                Parallel.ForEach(list, (item) =>
+                {
+                    Console.WriteLine(item);
+                });
+                Parallel.ForEach(longList, (item) =>
+                {
+                    Console.WriteLine(item);
+                });
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ParallelHomeWork/StringExtension.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParallelHomeWork
+{
+    public static class StringExtension
+    {
+        public static string SortStringLines(this string str)
+        {
+            string[] subStr = str.Split('\n');
+            for (int i = 0; i < subStr.Length - 2; i++)
+            {
+                for (int l = 0; l < subStr.Length - i - 2; l++)
+                {
+                    if (subStr[l][0] > subStr[l + 1][0])
+                    {
+                        string tmp = subStr[l];
+                        subStr[l] = subStr[l + 1];
+                        subStr[l + 1] = tmp;
+                    }
+                }
+            }
+            string tmp1 = "";
+            foreach (var item in subStr)
+            {
+                tmp1 += item;
+                tmp1 += '\n';
+            }
+            return tmp1;
+        }
+
+        public static List<int> ExtractNumbers(this string str)
+        {
+            string tmp = new string(str.Where(c => Char.IsDigit(c) || c == ' ').ToArray());
+            var list = tmp.Split(' ').Where(s => s != "").ToArray();
+            List<int> result = new List<int>();
+            foreach (var item in list)
+            {
+                result.Add(Int32.Parse(item));
+            }
+            return result;
+        }
+    }
+}
